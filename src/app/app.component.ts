30929d4c8fca870392ac91b79c3539437540b0ea
Sagar Thakre <sagarthakre159.com>
2025-09-06 21:56:00 +0530

    first change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { EventsComponent } from './components/events/events.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'',component:HomeComponent
+},
+{
+    path:'blogs', component:BlogsComponent
+},{
+    path:'events', component:EventsComponent
+
+},{
+    path:'aboutus',component:AboutusComponent
+}
+];
