a809c0b11a068d9039db0206c3ec5897a4e54cf1
Sagar Thakre <sagarthakre159.com>
2025-09-08 21:07:44 +0530

    fourth

--- src/app/components/blogs/blogs.component.ts
+++ src/app/components/blogs/blogs.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+interface Blog {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  date: string;
+  author: string;
+}
 
 @Component({
   selector: 'app-blogs',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './blogs.component.html',
   styleUrl: './blogs.component.css'
 })
-export class BlogsComponent {
+export class BlogsComponent implements OnInit {
+  allBlogs: Blog[] = [];
+  displayedBlogs: Blog[] = [];
+  currentPage = 1;
+  itemsPerPage = 8;
+  hasMoreBlogs = false;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadBlogData();
+  }
+
+  loadBlogData() {
+    this.http.get<{ blogs: Blog[] }>('assets/data/blogs.json').subscribe(data => {
+      this.allBlogs = data.blogs;
+      this.updateDisplayedBlogs();
+    });
+  }
+
+  updateDisplayedBlogs() {
+    const startIndex = 0;
+    const endIndex = this.currentPage * this.itemsPerPage;
+    this.displayedBlogs = this.allBlogs.slice(startIndex, endIndex);
+    this.hasMoreBlogs = endIndex < this.allBlogs.length;
+  }
+
+  loadMore() {
+    this.currentPage++;
+    this.updateDisplayedBlogs();
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  currentImageIndex = 0;
+  images = [
+    'nirma.jpeg',
+    'nirma2.png',
+    'nirma3.jpeg',
+    'bg.jpg',
+    'bg2.jpg',
+    'logo.png'
+  ];
 
+  nextImage() {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+  }
+
+  previousImage() {
+    this.currentImageIndex = (this.currentImageIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  setCurrentImage(index: number) {
+    this.currentImageIndex = index;
+  }
 }
